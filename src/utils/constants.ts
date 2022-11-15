9ef500be4d5345f1e5a7247228b5b6675f78ade0
LifeOfCoding <LifeOfCoding@gmail.com>
2022-11-15 15:48:25 -0500

    Make contract address env var

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import Contract from "../artifacts/contracts/NFT.sol/RousseauNFTs.json";
 const contractABI = Contract.abi;
-const contractAddress = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
+const contractAddress = process.env.CONTRACT_ADDRESS;
 const ethereum =
   typeof window !== "undefined" && window.hasOwnProperty("ethereum")
     ? (window as any).ethereum
